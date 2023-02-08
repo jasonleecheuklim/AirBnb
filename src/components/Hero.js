b11cfa791e0da245461cf25a9ff44b9b41aeb495
jasonleecheuklim <jasonleelim@gmail.com>
2023-02-08 23:31:05 +0800

    2023-02-08 11:31pm

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../style.css'
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="/images/photo-grid.png"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by
+                one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
